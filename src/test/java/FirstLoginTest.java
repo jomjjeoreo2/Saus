1caa07dc5de15ddab0dd75fec9c3a1b1769887c7
kseniia_kan <jomjjeoreo5@mail.ru>
2026-01-27 02:13:12 +0300

    test is added (unfinised version)

---
+++ src/test/java/FirstLoginTest.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class FirstLoginTest {
+    @Test
+    public void firstLogin() {
+        //открыть браузер
+        //зайти на сайт https://www.saucedemo.com/
+
+        System.setProperty("webdriver.crome.driver", "src/test/resources/chromedriver.exe");
+
+        WebDriver browser = new ChromeDriver();
+
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.id("user-name")).sendKeys("standard_user");
+        browser.findElement(By.id("user-name")).sendKeys(Keys.CONTROL + "A");
+        browser.findElement(By.id("user-name")).sendKeys(Keys.BACK_SPACE);
+        browser.findElement(By.id("user-name")).sendKeys("standard_user");
+        browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("secret_sauce");
+        browser.findElement(By.cssSelector("[name='login-button']")).click();
+
+        boolean titleIsDisplayed = browser.findElement(By.ByCssSelector("[data-test='title']")).isDisplayed();
+        assertTrue(titleIsDisplayed);
+
+        String titleName = browser.findElement(By.ByCssSelector("[data-test='title']")).getText();
+        assertEquals(titleName, "Products")
+
+
+        //browser.findElement(By.xpath("//*[@id='user-mame']"));
+        //browser.findElement(By.cssSelector("[id='user-mame']"));
+
+
+        //browser.quit();
+
+
+    }
+
+}
