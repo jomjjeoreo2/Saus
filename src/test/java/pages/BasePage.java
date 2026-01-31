5658ef96176b34f6dc128d574eaaa5e2c2e929ea
kseniia_kan <jomjjeoreo5@mail.ru>
2026-01-31 20:05:13 +0300

    pom's updated, cartpage's, carttest's added

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -8,11 +9,17 @@ import java.time.Duration;
 public abstract class BasePage {
     public static final String DATA_TEST_PATTERN = "[data-test='%s']";
     public static final String BASE_URL = "https://www.saucedemo.com/";
+    private final By title = By.cssSelector(DATA_TEST_PATTERN.formatted("title"));
 
     WebDriver driver;
     WebDriverWait wait;
+
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public String checkTitleName() {
+        return driver.findElement(title).getText();
     }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.CartPage;
 import pages.LoginPage;
 import pages.ProductsPage;
 
@@ -14,6 +15,7 @@ public class BaseTest {
     public WebDriver driver;
     LoginPage loginPage;
     ProductsPage productsPage;
+    CartPage cartPage;
 
     @BeforeMethod
     public void setup() {
@@ -29,6 +31,7 @@ public class BaseTest {
 
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
+        cartPage = new CartPage(driver);
 
     }
 
